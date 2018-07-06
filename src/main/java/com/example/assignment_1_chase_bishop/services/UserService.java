2dd1dedbecd27911262395f5c1c8201be36fa2f9
chasebish <bishop.c@husky.neu.edu>
2018-07-05 22:08:29 -0400

    added user-admin api

--- src/main/java/com/example/assignment_1_chase_bishop/services/UserService.java
+++ src/main/java/com/example/assignment_1_chase_bishop/services/UserService.java
@@ -2,8 +2,11 @@ package com.example.assignment_1_chase_bishop.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.assignment_1_chase_bishop.models.User;
@@ -11,17 +14,38 @@ import com.example.assignment_1_chase_bishop.repositories.UserRepository;
 
 @RestController
 public class UserService {
-	
+
 	@Autowired
-	UserRepository repository;
-	
-	@GetMapping("api/hello")
+	UserRepository userRepository;
+
+	@GetMapping("/api/user")
 	public Iterable<User> findAllUsers() {
-		return repository.findAll();
+		return userRepository.findAll();
+	}
+
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(@PathVariable("userId") int id) {
+		return userRepository.findById(id).orElse(null);
 	}
 	
-	@PostMapping("api/hello")
+	@PostMapping("/api/user")
 	public User createUser(@RequestBody User user) {
-		return repository.save(user);
+		return userRepository.save(user);
 	}
+
+	@PutMapping("/api/user")
+	public User updateUser(@PathVariable("userId") int id, @RequestBody User newUser) {
+		User user = userRepository.findById(id).orElse(null);
+		user.set(newUser);
+		return userRepository.save(user);
+	}
+
+//	@GetMapping("api/user")
+//	public Iterable<User> findAllUsers(@RequestParam(name = "username", required = false) String username) {
+//		if (username != null) {
+//			return userRepository.findUserByUsername(username);
+//		}
+//		return userRepository.findAll();
+//	}
+
 }
