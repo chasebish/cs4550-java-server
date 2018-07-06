222418786f3fc8e8e848797dd6f26803217f75be
chasebish <bishop.c@husky.neu.edu>
2018-07-05 22:21:29 -0400

    added user.service.client functionality

--- src/main/webapp/jquery/services/user.service.client.js
+++ src/main/webapp/jquery/services/user.service.client.js
@@ -6,9 +6,38 @@ function UserServiceClient() {
     this.updateUser = updateUser;
     this.url = 'http://localhost:8080/api/user';
     var self = this;
-    function createUser(user, callback) { … }
-    function findAllUsers(callback) { … }
-    function findUserById(userId, callback) { … }
-    function updateUser(userId, user, callback) { … }
-    function deleteUser(userId, callback) { … }
+
+    function createUser(user) {
+        return fetch(self.url, {
+            method: 'POST',
+            body: JSON.stringify(user)
+        });
+    }
+
+    function findAllUsers(callback) {
+        return $.ajax({
+            url: self.url,
+            success: callback
+        })
+    }
+
+    function findUserById(userId) {
+        return fetch(
+            self.url + '/' + userId);
+    }
+
+    function updateUser(userId, user) {
+        return fetch(self.url + '/' + userId, {
+            method: 'PUT',
+            body: JSON.stringify(user)
+        });
+    }
+
+    function deleteUser(userId, callback) {
+        return fetch(
+            self.url + '/' + userId,
+            { method: 'DELETE' }
+        );
+    }
+     
 }
