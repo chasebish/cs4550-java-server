ef8e66345aa75e5c6df29bf10a2f8b2aed06fbb4
chasebish <bishop.c@husky.neu.edu>
2018-07-09 15:54:43 -0400

    find user by id

--- src/main/java/com/example/assignment_1_chase_bishop/repositories/UserRepository.java
+++ src/main/java/com/example/assignment_1_chase_bishop/repositories/UserRepository.java
@@ -2,13 +2,10 @@ package com.example.assignment_1_chase_bishop.repositories;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.example.assignment_1_chase_bishop.models.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
-
-//	@Query("SELECT u FROM User u WHERE u.username=:username")
-//	Iterable<User> findUserByUsername(@Param("username") String u);
-
+	@Query("SELECT user FROM User user WHERE user.username=:username AND user.password=:password")
+	public User findUserByCredentials(String username, String password);
 }
--- src/main/java/com/example/assignment_1_chase_bishop/services/UserService.java
+++ src/main/java/com/example/assignment_1_chase_bishop/services/UserService.java
@@ -1,5 +1,7 @@
 package com.example.assignment_1_chase_bishop.services;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,13 @@ public class UserService {
 
 	@Autowired
 	UserRepository userRepository;
+	
+	@PostMapping("/register")
+	public User register(@RequestBody User user, HttpSession session) {
+		User currentUser = userRepository.save(user);
+		session.setAttribute("currentUser", currentUser);
+		return currentUser;
+	}
 
 	@GetMapping("/api/user")
 	public Iterable<User> findAllUsers() {
