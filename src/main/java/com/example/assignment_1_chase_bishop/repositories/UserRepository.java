8d55fb64dda732c455401dcf55955b0620b0ba3e
chasebish <bishop.c@husky.neu.edu>
2018-07-09 23:25:48 -0400

    register page

--- src/main/java/com/example/assignment_1_chase_bishop/repositories/UserRepository.java
+++ src/main/java/com/example/assignment_1_chase_bishop/repositories/UserRepository.java
@@ -8,4 +8,7 @@ import com.example.assignment_1_chase_bishop.models.User;
 public interface UserRepository extends CrudRepository<User, Integer> {
 	@Query("SELECT user FROM User user WHERE user.username=:username AND user.password=:password")
 	public User findUserByCredentials(String username, String password);
+	
+	@Query("SELECT user FROM User user WHERE user.username=:username")
+	public User findUserByUsername(String username);
 }
