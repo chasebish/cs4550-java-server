acd8de92f003d67140ca6d5cf5638dc8d009ce26
chasebish <bishop.c@husky.neu.edu>
2018-07-30 12:47:46 -0400

    added user cors

--- src/main/java/com/example/assignment_1_chase_bishop/services/UserService.java
+++ src/main/java/com/example/assignment_1_chase_bishop/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.example.assignment_1_chase_bishop.repositories.UserRepository;
 
 // Service for homework
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class UserService {
 
 	@Autowired
